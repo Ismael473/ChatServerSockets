ee26b37327a182b726e376ff497dfa6adbcbc658
Ismael473 <62965318+Ismael473@users.noreply.github.com>
2022-03-10 20:48:04 -0600

    Modificaciones a la pagina principal

--- Tarea Extraclase 1/src/InterfaceDeUsuario/PaginaPrincipal.java
+++ Tarea Extraclase 1/src/InterfaceDeUsuario/PaginaPrincipal.java
@@ -14,11 +14,18 @@ public class PaginaPrincipal {
         JButton botSal = new JButton("Salir");
         botSal.setBounds(450,500,100,40);
 
-        JTextField mensaje = new JTextField();
-        mensaje.setBounds(0,500, 400,20);
+        JTextField mensajeAEnviar = new JTextField();
+        mensajeAEnviar.setBounds(0,500, 400,20);
 
+        JLabel mensaje = new JLabel((Icon) mensajeAEnviar);
+        mensaje.setBounds(0,100,40,20);
+
+        mensajeAEnviar.setVisible(true);
+        mensaje.setVisible(true);
         botEnv.setVisible(true);
         botSal.setVisible(true);
+        pant.add(mensaje);
+        pant.add(mensajeAEnviar);
         pant.add(botEnv);
         pant.add(botSal);
         pant.setSize(600,600);
